a0c2368e16a902f55603c8e779d0796c11ab3ba2
drsakr86-hash <dr.sakr86@gmail.com>
2026-09-14 23:54:02 +0300

    Delete app/components directory

--- app/components/ui/index.ts
+++
@@ -1,8 +0,0 @@
-export { Button } from './Button';
-export { Card } from './Card';
-export { Field, Input } from './Field';
-export { Spinner } from './Spinner';
-export { Tag } from './Tag';
-export { Toasts } from './Toasts';
-export { Modal } from './Modal';
-export { EmptyState } from './EmptyState';
